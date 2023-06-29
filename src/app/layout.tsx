6848636930f3a4c07ae081aeac4c9f03ea0fa164
Felipe Fett <felipecaldasfett@gmail.comm>
2023-06-29 11:42:27 -0300

    chore: configs metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,34 +1,40 @@
 import './globals.css'
 
 import classNames from 'classnames'
+import { Metadata } from 'next'
 import { Caveat, Roboto_Slab, Work_Sans } from 'next/font/google'
 import Script from 'next/script'
 import React from 'react'
+
 const robotoSlab = Roboto_Slab({
+  display: 'swap',
   preload: true,
-  /* display: 'swap', */
   subsets: ['latin'],
   weight: ['300', '400', '600'],
 })
 
 const workSans = Work_Sans({
+  display: 'swap',
   preload: true,
-  /* display: 'swap', */
   subsets: ['latin'],
   weight: ['300', '400'],
 })
 
 const caveat = Caveat({
+  display: 'swap',
   preload: true,
-  /* display: 'swap', */
   subsets: ['latin'],
   weight: ['700'],
 })
-export const metadata = {
+export const metadata: Metadata = {
   title: '@fcfett',
-  description: "Nice to meet you! I'm Fett",
+  description: "Nice to meet you! I'm Fett.",
+  openGraph: {
+    type: 'website',
+    images:
+      'https://raw.githubusercontent.com/fcfett/resources/master/profile.jpg',
+  },
 }
-
 type RootLayoutProps = {
   children: React.ReactNode
 }
